2d48d07217716ac1d5af31815f426efdb22f3839
Yomi Ikuru <abayomi.ikuru@gmail.com>
2022-03-11 15:56:53 +0000

    Made changes

--- suspend Extension/Resources/background.js
+++ suspend Extension/Resources/background.js
@@ -1,6 +1,14 @@
+let state = {
+  "global": "global state"
+}
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 
     if (request.greeting === "hello")
         sendResponse({ farewell: "goodbye" });
 });
+
+browser.runtime.onInstalled(() => {
+  browser.storage.local.set()
+})
--- suspend Extension/Resources/popup.js
+++ suspend Extension/Resources/popup.js
@@ -1,1 +1,18 @@
 console.log("Hello World!", browser);
+
+const optionsButton = document.getElementById("options-button")
+
+optionsButton.innerHTML = '<img src="images/assets/options-icon2.svg"/>';
+
+var optionsUrl = chrome.extension.getURL('options.html');
+
+optionsButton.onclick = function () {
+  console.log("Options button clicked")
+  const optionsUrl = browser.extension.getURL('options.html');
+  document.tabs.create({ url: optionsUrl })
+  console.log("Options button clicked")
+}
+
+function handleButtonClick(e) {
+  
+}
